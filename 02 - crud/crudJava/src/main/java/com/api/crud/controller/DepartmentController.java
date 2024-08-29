e75cf04d706a8ee10e826abd6156f5d8f2d49829
leona <leonardovieiraxy@hotmail.com>
2024-08-29 10:29:09 -0300

    Changes to be committed: 	new file:   02 - crud/crud/.gitignore 	new file:   02 - crud/crud/.mvn/wrapper/maven-wrapper.properties 	new file:   02 - crud/crud/mvnw 	new file:   02 - crud/crud/mvnw.cmd 	new file:   02 - crud/crud/pom.xml 	new file:   02 - crud/crud/src/main/java/com/api/crud/CrudApplication.java 	new file:   02 - crud/crud/src/main/java/com/api/crud/controller/ProdutoController.java 	new file:   02 - crud/crud/src/main/java/com/api/crud/model/Produto.java 	new file:   02 - crud/crud/src/main/java/com/api/crud/repository/ProdutoRepository.java 	new file:   02 - crud/crud/src/main/java/com/api/crud/service/ProdutoService.java 	new file:   02 - crud/crud/src/main/resources/application.properties 	new file:   02 - crud/crud/src/test/java/com/api/crud/CrudApplicationTests.java 	modified:   02 - crud/crudJava/pom.xml 	new file:   02 - crud/crudJava/src/main/java/com/api/crud/controller/DepartmentController.java 	new file:   02 - crud/crudJava/src/main/java/com/api/crud/controller/EmployeeController.java 	modified:   02 - crud/crudJava/src/main/java/com/api/crud/controller/ProdutoController.java 	new file:   02 - crud/crudJava/src/main/java/com/api/crud/model/Department.java 	new file:   02 - crud/crudJava/src/main/java/com/api/crud/model/Employee.java 	new file:   02 - crud/crudJava/src/main/java/com/api/crud/repository/DepartmentRepository.java 	new file:   02 - crud/crudJava/src/main/java/com/api/crud/repository/EmployeeRepository.java 	new file:   02 - crud/crudJava/src/main/java/com/api/crud/service/DepartmentService.java 	new file:   02 - crud/crudJava/src/main/java/com/api/crud/service/EmployeeService.java 	modified:   02 - crud/crudJava/src/main/java/com/api/crud/service/ProdutoService.java 	modified:   02 - crud/crudJava/src/main/resources/application.properties

---
+++ 02 - crud/crudJava/src/main/java/com/api/crud/controller/DepartmentController.java
@@ -0,0 +1,62 @@
+package com.api.crud.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.crud.model.Department;
+import com.api.crud.service.DepartmentService;
+
+
+
+
+@RestController
+@RequestMapping("/api/departments")
+public class DepartmentController {
+    @Autowired
+    private DepartmentService departmentService;
+
+    @GetMapping
+    public List<Department> getAllDepartments() {
+        return departmentService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Department> getDepartmentById(@PathVariable Long id) {
+        Optional<Department> department = departmentService.findById(id);
+        return department.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Department createDepartment(@RequestBody Department department) {
+        return departmentService.save(department);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Department> updateDepartment(@PathVariable Long id, @RequestBody Department departmentDetails) {
+        Optional<Department> department = departmentService.findById(id);
+        if (department.isPresent()) {
+            Department updatedDepartment = department.get();
+            updatedDepartment.setTitle(departmentDetails.getTitle());
+            return ResponseEntity.ok(departmentService.save(updatedDepartment));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDepartment(@PathVariable Long id) {
+        departmentService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ 02 - crud/crudJava/src/main/java/com/api/crud/service/EmployeeService.java
@@ -0,0 +1,34 @@
+package com.api.crud.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.crud.model.Employee;
+import com.api.crud.repository.EmployeeRepository;
+
+
+
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    public Optional<Employee> findById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteById(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
