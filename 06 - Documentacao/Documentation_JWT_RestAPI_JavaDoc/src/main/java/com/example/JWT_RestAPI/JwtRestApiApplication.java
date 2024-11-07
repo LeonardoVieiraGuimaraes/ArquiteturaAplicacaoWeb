0b719ea2bc59b9ab760f750824c9597b2b57d64d
Leonardo Vieira Guimarães <leonardovieiraxy@hotmail.com>
2024-11-07 16:01:07 -0300

    Changes to be committed: 	modified:   06 - Documentacao/Documentation_JWT_RestAPI_JavaDoc/src/main/java/com/example/JWT_RestAPI/JwtRestApiApplication.java

--- 06 - Documentacao/Documentation_JWT_RestAPI_JavaDoc/src/main/java/com/example/JWT_RestAPI/JwtRestApiApplication.java
+++ 06 - Documentacao/Documentation_JWT_RestAPI_JavaDoc/src/main/java/com/example/JWT_RestAPI/JwtRestApiApplication.java
@@ -5,12 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Classe principal para iniciar a aplicação JWT Rest API com Spring Boot.
+ * <p>
+ * Esta classe contém o método principal que é o ponto de entrada da aplicação Spring Boot.
+ * A anotação {@link SpringBootApplication} é usada para habilitar a configuração automática,
+ * a varredura de componentes e outras funcionalidades do Spring Boot.
+ * </p>
  */
 @SpringBootApplication(scanBasePackages = {"com.example"})
 public class JwtRestApiApplication {
 
     /**
      * Método principal que inicia a aplicação Spring Boot.
+     * <p>
+     * Este método chama {@link SpringApplication#run(Class, String...)} para iniciar a aplicação.
+     * </p>
      *
      * @param args argumentos da linha de comando.
      */
