a10da787772079725b460fdbad75776d7c6c338e
LeonardoVieiraGuimaraes <leonardovieiraxy@hotmail.com>
2025-04-10 19:31:43 -0300

    Changes to be committed: 	modified:   Aula05/H2/democrud/src/main/java/com/example/democrud/controller/ProdutoController.java 	modified:   Aula05/H2/democrud/src/main/java/com/example/democrud/model/Produto.java 	modified:   Aula05/H2/democrud/src/main/java/com/example/democrud/service/ProdutoService.java 	modified:   Aula05/H2/democrud/src/main/resources/db/bancoDeDados.mv.db

--- Aula05/H2/democrud/src/main/java/com/example/democrud/model/Produto.java
+++ Aula05/H2/democrud/src/main/java/com/example/democrud/model/Produto.java
@@ -1,7 +1,8 @@
 package com.example.democrud.model;
 
 // Importações necessárias para o mapeamento e funcionamento da entidade JPA.
-import org.springframework.boot.autoconfigure.domain.EntityScan; // Configura o escaneamento de entidades.
+
+// Configura o escaneamento de entidades.
 import jakarta.persistence.Entity; // Marca a classe como uma entidade JPA.
 import jakarta.persistence.GeneratedValue; // Gera valores automaticamente para campos.
 import jakarta.persistence.GenerationType; // Define a estratégia de geração de valores.
@@ -10,8 +11,6 @@ import lombok.AllArgsConstructor; // Gera automaticamente um construtor com todo
 import lombok.Data; // Gera automaticamente getters, setters, equals, hashCode e toString.
 import lombok.NoArgsConstructor;
 
-// Configura o escaneamento de entidades no pacote especificado.
-@EntityScan("com.example.democrud.model")
 
 // Marca a classe como uma entidade JPA, permitindo que ela seja mapeada para uma tabela no banco de dados.
 @Entity
